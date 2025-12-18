6253d5577eac3f59c9b465250553540c7de00085
alex bravp <fabriciobravo373@gmail.com>
2025-12-18 17:54:12 -0500

    Problema 4

--- Taller10/src/paquete4/Problema04.java
+++ Taller10/src/paquete4/Problema04.java
@@ -15,11 +15,59 @@ public class Problema04 {
         // TODO code application logic here
        Scanner sc = new Scanner(System.in);
 
-       
+        double[][] matrizA = new double[3][2];
+        double[][] matrizB = new double[3][2];
+
+        System.out.println(" Ingreso Matriz A (3x2) ");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 2; j++) {
+                System.out.print("A[" + i + "][" + j + "]: ");
+                matrizA[i][j] = sc.nextDouble();
+            }
+        }
+        System.out.println("\n Ingreso Matriz B (3x2) ");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 2; j++) {
+                System.out.print("B[" + i + "][" + j + "]: ");
+                matrizB[i][j] = sc.nextDouble();
+            }
+        }
+        System.out.println("\nMATRIZ A:");
+        imprimirMatriz(matrizA);
+        System.out.println("\nMATRIZ B:");
+        imprimirMatriz(matrizB);
+        boolean todasMayorIgual = true;
+        boolean alMenosUnaEstricta = false;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 2; j++) {
+                if (!(matrizA[i][j] >= matrizB[i][j])) {
+                    todasMayorIgual = false;
+                }
+                if (matrizA[i][j] > matrizB[i][j]) {
+                    alMenosUnaEstricta = true;
+                }
+            }
+        }
+        System.out.println("\n RESULTADO DE LA COMPARACIÓN ");
+        if (todasMayorIgual && alMenosUnaEstricta) {
+            System.out.println("La matriz A es mayor que la matriz B");
+        } else {
+            System.out.println("La matriz A no es mayor que la matriz B");
+        }
+    }
+    public static void imprimirMatriz(double[][] m) {
+        for (double[] fila : m) {
+            for (double valor : fila) {
+                System.out.print(valor + "\t");
+            }
+            System.out.println();
+        }
+    }
+}
        
        
        
        
      
-     }
-}
+     
+
---
+++ Taller10/src/paquete7/Problema07.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paquete7;
+import java.util.Scanner;
+/**
+ *
+ * @author reroes
+ */
+public class Problema07 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        double[][] matrizX = new double[3][3];
+        double[][] matrizA = new double[3][3];
+        double[][] matrizB = new double[3][3];
+
+        System.out.println(" Ingreso de datos para la Matriz X (3x3) ");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print("X[" + i + "][" + j + "]: ");
+                matrizX[i][j] = sc.nextDouble();
+                double x = matrizX[i][j];
+                matrizA[i][j] = (x + 1) * (x + 1);
+                matrizB[i][j] = (x * x) + (2 * x) + 1;
+            }
+        }
+        boolean identidadCumplida = true;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (matrizA[i][j] != matrizB[i][j]) {
+                    identidadCumplida = false;
+                    break;
+                }
+            }
+        }
+        System.out.println("\n RESULTADO DE LA IDENTIDAD ");
+        if (identidadCumplida) {
+            System.out.println("La identidad algebraica se cumple.");
+        } else {
+            System.out.println("La identidad algebraica NO se cumple.");
+        }
+    }
+}
