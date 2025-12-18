1a533128a1a77e662769670a69309ff2a70af2de
alex bravp <fabriciobravo373@gmail.com>
2025-12-18 11:01:02 -0500

    Problema 1

--- Taller10/src/paquete1/Problema01.java
+++ Taller10/src/paquete1/Problema01.java
@@ -4,6 +4,8 @@
  */
 package paquete1;
 
+import java.util.Scanner;
+
 /**
  *
  * @author reroes
@@ -15,6 +17,34 @@ public class Problema01 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+
+        boolean[][] asistencia = {
+            {true, true, false, true, true},
+            {true, false, false, true, false},
+            {true, true, true, true, true},
+            {false, true, true, false, true}
+        };
+        String[] estudiantes = {"Ana", "Luis", "Carlos", "María"};
+        int[] diasAsistidos = new int[4];
+
+        System.out.println("Reporte de Asistencia");
+
+        for (int i = 0; i < asistencia.length; i++) {
+            int contador = 0;
+            for (int j = 0; j < asistencia[i].length; j++) {
+                if (asistencia[i][j]) {
+                    contador++;
+                }
+            }
+            diasAsistidos[i] = contador;
+            System.out.print(estudiantes[i] + ": " + diasAsistidos[i] + " días.");
+            if (diasAsistidos[i] == 5){
+            System.out.println("Asistencia completa");
+        }else {
+             System.out.println("Asistencia incompleta");
+             }
+        }   
+
     }
-    
 }
